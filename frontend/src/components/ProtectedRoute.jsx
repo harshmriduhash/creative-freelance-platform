d2dfeee9f91fb2847fe3320b7191ac3667f007e0
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-10-27 23:10:22 +0530

    navigation route

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
-import { Navigate } from 'react-router-dom';
-import useAuthStore from '../store/authStore';
+import { Navigate } from "react-router-dom";
+import useAuthStore from "../store/authStore";
 
 function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuthStore();
