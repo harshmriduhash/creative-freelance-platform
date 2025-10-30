1b74346f9b9844b8e8b3e0dd430ca0b4055c7aee
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-10-30 11:05:13 +0530

    projects issue resolved

--- backend/src/routes/projects.js
+++ backend/src/routes/projects.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const projectController = require('../controllers/projectController');
-const { protect } = require('../middleware/auth');
+const express = require("express");
+const projectController = require("../controllers/projectController");
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
 // All project routes require authentication
 router.use(protect);
 
-router.get('/', projectController.getProjects);
-router.get('/:id', projectController.getProject);
-router.post('/', projectController.createProject);
-router.put('/:id/milestone/:milestoneId', projectController.updateMilestone);
-router.post('/:id/complete', projectController.completeProject);
-router.post('/:id/review', projectController.submitReview);
+router.get("/", projectController.getProjects);
+router.get("/:id", projectController.getProject);
+router.post("/", projectController.createProject);
+router.put("/:id/milestone/:milestoneId", projectController.updateMilestone);
+router.post("/:id/complete", projectController.completeProject);
+router.post("/:id/review", projectController.submitReview);
 
 module.exports = router;
