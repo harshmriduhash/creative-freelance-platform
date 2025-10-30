417a1614bbc4df5a35f1c3aed976205b0c4273ef
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-10-30 11:03:36 +0530

    esporting module

--- backend/src/routes/gigs.js
+++ backend/src/routes/gigs.js
@@ -1,15 +1,25 @@
-const express = require('express');
-const gigController = require('../controllers/gigController');
-const { protect, authorize } = require('../middleware/auth');
+const express = require("express");
+const gigController = require("../controllers/gigController");
+const { protect, authorize } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get('/', gigController.getGigs);
-router.get('/:id', gigController.getGig);
-router.post('/', protect, authorize('client', 'admin'), gigController.createGig);
-router.put('/:id', protect, gigController.updateGig);
-router.delete('/:id', protect, gigController.deleteGig);
-router.post('/:id/bid', protect, authorize('freelancer'), gigController.placeBid);
-router.get('/:id/bids', protect, gigController.getGigBids);
+router.get("/", gigController.getGigs);
+router.get("/:id", gigController.getGig);
+router.post(
+  "/",
+  protect,
+  authorize("client", "admin"),
+  gigController.createGig
+);
+router.put("/:id", protect, gigController.updateGig);
+router.delete("/:id", protect, gigController.deleteGig);
+router.post(
+  "/:id/bid",
+  protect,
+  authorize("freelancer"),
+  gigController.placeBid
+);
+router.get("/:id/bids", protect, gigController.getGigBids);
 
 module.exports = router;
